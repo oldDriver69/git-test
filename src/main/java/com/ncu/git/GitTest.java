57ec27c8a0ae60c22e3d090946a8fd7806492a33
hezongyi <1320219969@qq.com>
2022-03-11 13:17:57 +0800

    first commit

---
+++ src/main/java/com/ncu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.ncu.git;
+
+/**
+ * @description:
+ * @author: heZongYi
+ * @create: 2022-03-11 12:28
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("k");
+    }
+}
